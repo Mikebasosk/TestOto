756001cbf05e8afd31c1ee6d92365f00bb498ed2
Mikebasosk <37615508+Mikebasosk@users.noreply.github.com>
2018-03-21 16:35:58 +0100

    finally triangolo
    modifica ok

--- src/plm/oto/Test/Triangolo.java
+++ src/plm/oto/Test/Triangolo.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package plm.oto.Test;
 
 public class Triangolo {
@@ -34,48 +34,8 @@ public class Triangolo {
 	}
 
 	public void Area(){
-		double pd2 = this.Perimetro()/2;
+		double pd2 = this.perimetro/2;
 		this.area =Math.sqrt(pd2*(pd2-this.l1)*(pd2-this.l2)*(pd2-this.l3));
 	}
 }
-=======
-package plm.oto.Test;
 
-public class Triangolo {
-	
-	/**
-	 * 	Classe da impostare di Davide
-	 */
-	
-	private double l1, l2, l3;
-	
-	public double getLato1(){
-		return this.l1;
-	}
-	public double getLato2(){
-		return this.l2;
-	}
-	public double getLato3(){
-		return this.l3;
-	}
-	
-	public void setLato1(double l){
-		this.l1 = l;
-	}
-	public void setLato2(double l){
-		this.l2 = l;
-	}
-	public void setLato3(double l){
-		this.l3 = l;
-	}
-	
-	public double Perimetro(){
-		return this.l1+this.l2+this.l3;
-	}
-	
-	public double Area(){
-		double pd2 = this.Perimetro()/2;
-		return Math.sqrt(pd2*(pd2-this.l1)*(pd2-this.l2)*(pd2-this.l3));
-	}
-}
->>>>>>> e2a68f852d1493d7773b8d3279f874f65b59765d
