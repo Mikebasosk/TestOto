5713ac35723830c9b83f2f11337f1119ea219129
Mikebasosk <37615508+Mikebasosk@users.noreply.github.com>
2018-03-21 16:32:26 +0100

    triangolo ok
    Dave ora funziona bene il triangolo.

--- src/plm/oto/Test/Triangolo.java
+++ src/plm/oto/Test/Triangolo.java
@@ -1,13 +1,13 @@
 package plm.oto.Test;
 
 public class Triangolo {
-	
+
 	/**
 	 * 	Classe da impostare di Davide
 	 */
-	
-	private double l1, l2, l3;
-	
+
+	private double l1, l2, l3, area, perimetro;
+
 	public double getLato1(){
 		return this.l1;
 	}
@@ -17,7 +17,7 @@ public class Triangolo {
 	public double getLato3(){
 		return this.l3;
 	}
-	
+
 	public void setLato1(double l){
 		this.l1 = l;
 	}
@@ -27,13 +27,13 @@ public class Triangolo {
 	public void setLato3(double l){
 		this.l3 = l;
 	}
-	
-	public double Perimetro(){
-		return this.l1+this.l2+this.l3;
+
+	public void Perimetro(){
+		this.perimetro = this.l1+this.l2+this.l3;
 	}
-	
-	public double Area(){
+
+	public void Area(){
 		double pd2 = this.Perimetro()/2;
-		return Math.sqrt(pd2*(pd2-this.l1)*(pd2-this.l2)*(pd2-this.l3));
+		this.area =Math.sqrt(pd2*(pd2-this.l1)*(pd2-this.l2)*(pd2-this.l3));
 	}
 }
