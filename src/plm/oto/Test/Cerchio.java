7335db78dcb31e8b048ccce457a21f555b88e927
IvanCostantino <37615789+IvanCostantino@users.noreply.github.com>
2018-03-21 15:44:58 +0100

    aggiunta metodi alla classe Cerchio

--- src/plm/oto/Test/Cerchio.java
+++ src/plm/oto/Test/Cerchio.java
@@ -3,4 +3,26 @@ package plm.oto.Test;
 public class Cerchio {
 	private double raggio;
 	final double  PIGreco = 3.14;
+
+	public Cerchio (){
+
+	}
+
+	public Cerchio (double raggio){
+		this.raggio = raggio;
+	}
+
+	public double getRaggio() {
+		return raggio;
+	}
+
+	public void setRaggio(double raggio) {
+		this.raggio = raggio;
+	}
+
+	public String toString() {
+		return "Cerchio [raggio=" + raggio + ", PIGreco=" + PIGreco + "]";
+	}
+
+
 }
