e15252c43cede041f419c07821c6a47cfa5ec63c
raadi92 <raadi92@users.noreply.github.com>
2018-03-21 16:27:19 +0100

    Finita Triangolo

--- src/plm/oto/Test/Triangolo.java
+++ src/plm/oto/Test/Triangolo.java
@@ -6,5 +6,34 @@ public class Triangolo {
 	 * 	Classe da impostare di Davide
 	 */
 	
+	private double l1, l2, l3;
 	
+	public double getLato1(){
+		return this.l1;
+	}
+	public double getLato2(){
+		return this.l2;
+	}
+	public double getLato3(){
+		return this.l3;
+	}
+	
+	public void setLato1(double l){
+		this.l1 = l;
+	}
+	public void setLato2(double l){
+		this.l2 = l;
+	}
+	public void setLato3(double l){
+		this.l3 = l;
+	}
+	
+	public double Perimetro(){
+		return this.l1+this.l2+this.l3;
+	}
+	
+	public double Area(){
+		double pd2 = this.Perimetro()/2;
+		return Math.sqrt(pd2*(pd2-this.l1)*(pd2-this.l2)*(pd2-this.l3));
+	}
 }
