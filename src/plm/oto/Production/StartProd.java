11ea100e43210b6e0989efc8f90877913e4f967c
mikebasosk <mike.basosk@gmail.com>
2018-03-21 12:51:31 +0100

    Change comment
    from Todo to Dave

--- src/plm/oto/Production/StartProd.java
+++ src/plm/oto/Production/StartProd.java
@@ -3,7 +3,6 @@ package plm.oto.Production;
 public class StartProd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/plm/oto/Test/StartTest.java
+++ src/plm/oto/Test/StartTest.java
@@ -3,8 +3,8 @@ package plm.oto.Test;
 public class StartTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Dave no good
 	}
 
 }
